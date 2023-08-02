fe0789d923928b32524f4847a15fd58fc4cf72d1
EngEmadHany <emadhany2007@gmail.com>
2023-08-02 23:23:44 +0400

    Initial Commit

---
+++ SwiftUIScrollView/SwiftUIScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIScrollViewApp.swift
+//  SwiftUIScrollView
+//
+//  Created by Emad Hany Isaac on 11/3/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
